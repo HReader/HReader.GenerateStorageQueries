ea9d857a97f443fa7fe449bcf468379ff95ebe9e
HReader <hreader@mailbox.org>
2017-12-01 22:55:17 +0100

    Add project files.

---
+++ HReader.GenerateStorageQueries/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HReader.GenerateStorageQueries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
